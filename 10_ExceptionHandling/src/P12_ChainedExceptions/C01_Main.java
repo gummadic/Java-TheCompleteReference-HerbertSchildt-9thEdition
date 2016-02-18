e3c4799a9e032d2b6f03458dee58fc6b3933dd95
Raven-Pro <m_117@outlook.com>
2016-02-18 15:48:56 -0600

    Add chained exceptions example

---
+++ 10_ExceptionHandling/src/P12_ChainedExceptions/C01_Main.java
@@ -0,0 +1,25 @@
+package P12_ChainedExceptions;
+
+public class C01_Main {
+    static void demoproc() {
+        // Create an exception.
+        NullPointerException e = new NullPointerException("top layer");
+        
+        // Add a cause.
+        e.initCause(new ArithmeticException("cause"));
+        
+        throw e;
+    }
+    
+    public static void main(String args[]) {
+        try {
+            demoproc();
+        } catch(NullPointerException e) {
+            // Display top level exception.
+            System.out.println("Caught: " + e);
+            
+            // Display cause exception.
+            System.out.println("Original cause: " + e.getCause());
+        }
+    }
+}
