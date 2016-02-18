30d95da27bacc48f934e7259f31033bf93a8674f
Raven-Pro <m_117@outlook.com>
2016-02-18 16:58:12 -0600

    Initial commit for packages and interfaces

---
+++ 09_PackagesAndInterfaces/src/P01_Packages/C01_Main.java
@@ -0,0 +1,14 @@
+package P01_Packages;
+
+public class C01_Main {
+    String name;
+    double bal;
+
+    void show() {
+        if(val < 0) {
+            System.out.print("--> ");
+        }
+        
+        System.out.println(name + ": $" + bal);
+    }
+}
