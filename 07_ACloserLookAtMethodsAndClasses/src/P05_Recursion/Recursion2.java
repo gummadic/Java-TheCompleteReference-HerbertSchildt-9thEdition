381fef0f84a7e0d173ee2501c048fc93e93fc4a7
raven <raven@192.168.0.4>
2016-03-09 22:48:45 -0600

    Add recursion examples

---
+++ 07_ACloserLookAtMethodsAndClasses/src/P05_Recursion/Recursion2.java
@@ -0,0 +1,35 @@
+package P05_Recursion;
+
+// Another example that uses recursion.
+
+class RecTest {
+    int values[];
+    
+    RecTest(int i) {
+        values = new int[i];
+    }
+    
+    // Display arra -- recursively.
+    void printArray(int i) {
+        if(i == 0) {
+            return;
+        } else {
+            printArray(i - 1);
+        }
+        
+        System.out.println("[" + (i - 1) + "] " + values[i - 1]);
+    }
+}
+
+public class Recursion2 {
+    public static void main(String args[]) {
+        RecTest ob = new RecTest(10);
+        int i;
+        
+        for(i = 0; i < 10; i++) {
+            ob.values[i] = i;
+        }
+        
+        ob.printArray(10);
+    }
+}
