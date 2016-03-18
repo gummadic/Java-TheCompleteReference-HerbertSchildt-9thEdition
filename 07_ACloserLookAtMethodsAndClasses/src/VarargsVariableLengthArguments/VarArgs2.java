0e4d7a48c8c9235faa78217115a0688b42f6f56f
Raven-Pro <m_117@outlook.com>
2016-03-18 14:13:29 -0600

    Add varargs: variable-length arguments examples

---
+++ 07_ACloserLookAtMethodsAndClasses/src/VarargsVariableLengthArguments/VarArgs2.java
@@ -0,0 +1,21 @@
+package VarargsVariableLengthArguments;
+
+// Use varargs with standard arguments.
+public class VarArgs2 {
+    // Here, msg is a normal parameter and v is a
+    // vaargs parameter.
+    static void vaTest(String msg, int ... v) {
+        System.out.print(msg + v.length + " Contents: ");
+        
+        for(int x : v)
+            System.out.print(x + " ");
+        
+        System.out.println();
+    }
+    
+    public static void main(String args[]) {
+        vaTest("One varargs: ", 10);
+        vaTest("Three varargs: ", 1, 2, 3);
+        vaTest("No varargs: ");
+    }
+}
