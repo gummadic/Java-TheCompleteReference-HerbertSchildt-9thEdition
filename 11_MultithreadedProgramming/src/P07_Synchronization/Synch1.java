27780af039d0cf9d728f057c0afdef9cb48c75ef
Raven-Pro <m_117@outook.com>
2016-03-22 17:09:10 -0600

    Add examples for: Synchronization

---
+++ 11_MultithreadedProgramming/src/P07_Synchronization/Synch1.java
@@ -0,0 +1,53 @@
+package P07_Synchronization;
+
+// This program uses a synchronized block.
+class Callme2 {
+    void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {
+            System.out.println("Interrupted.");
+        }
+        
+        System.out.println("]");
+    }
+}
+
+class Caller2 implements Runnable {
+    String msg;
+    Callme2 target;
+    Thread t;
+    
+    public Caller2(Callme2 targ, String s) {
+        target = targ;
+        msg = s;
+        t = new Thread(this);
+        t.start();
+    }
+    
+    // Synchronize calls to call().
+    public void run() {
+        synchronized(target) { // Synchronized block.
+            target.call(msg);
+        }
+    }
+}
+
+public class Synch1 {
+    public static void main(String args[]) {
+        Callme2 target = new Callme2();
+        Caller2 ob1 = new Caller2(target, "Hello");
+        Caller2 ob2 = new Caller2(target, "Synchronized");
+        Caller2 ob3 = new Caller2(target, "World");
+        
+        // Wait for threads to end.
+        try {
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        } catch(InterruptedException e) {
+            System.out.println("Interrupted.");
+        }
+    }
+}
