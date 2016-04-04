aa5099521559986c7c9ba0d5eeccb66470834044
Raven-Pro <m_117@outlook.com>
2016-04-04 15:47:26 -0500

    Add example for: Marker Annotations

---
+++ 12_EnumerationsAutoboxingAndAnnotationsMetadata/src/P04_AnnotationsMetadata/Marker.java
@@ -0,0 +1,32 @@
+package P04_AnnotationsMetadata;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+
+// A merker annotation.
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyMarker{}
+
+public class Marker {
+    // Annotate a methodrke.
+    // Notice that no ( ) is needed.
+    @MyMarker
+    public static void myMeth() {
+        Marker ob = new Marker();
+        
+        try {
+            Method m = ob.getClass().getMethod("myMeth");
+            
+            // Determine if the annotation is present.
+            if(m.isAnnotationPresent(MyMarker.class)) {
+                System.out.println("MyMarker is present.");
+            }
+        } catch(NoSuchMethodException exc) {
+            System.out.println("Method Not Found.");
+        }
+    }
+    
+    public static void main(String args[]) {
+        myMeth();
+    }
+}
