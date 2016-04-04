8106f753f4c2fd48ce36d6fd7aaf4578d678c367
Raven-Pro <m_117@outlook.com>
2016-04-04 14:47:29 -0500

    Add example for: Autoboxing/Unboxing Boolean and Character Values

---
+++ 12_EnumerationsAutoboxingAndAnnotationsMetadata/src/P03_Autoboxing/AutoBox5.java
@@ -0,0 +1,22 @@
+package P03_Autoboxing;
+
+// Autoboxing/unboxing a Boolean and Character.
+
+public class AutoBox5 {
+    public static void main(String args[]) {
+        // Autobox/unbox a boolean.
+        Boolean b = true;
+        
+        // Below, b is auto-unboxed when used in
+        // a conditional expression, such as an if.
+        if(b) {
+            System.out.println("b is true");
+        }
+        
+        // Autobox/unbox a char.
+        Character ch = 'x'; // Box a char.
+        char ch2 = ch; // Unbox a char.
+        
+        System.out.println("ch2 is: " + ch2);
+    }
+}
