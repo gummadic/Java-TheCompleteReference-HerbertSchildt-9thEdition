9eb51cec22af3c260285d23ad75202dcc2eba249
Raven-Pro <m_117@outlook.com>
2016-04-18 16:23:01 -0500

    Add examples for: Reading Console Input

---
+++ Chapter_13/src/P02_ReadingConsoleInput/BRReader.java
@@ -0,0 +1,18 @@
+package P02_ReadingConsoleInput;
+// Use a BufferedReader to read characters from the console.
+import java.io.*;
+
+
+public class BRReader {
+    public static void main(String args[]) throws IOException {
+        char c;
+        BufferedReader br = new
+            BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter characters, 'q' to quit.");
+        // Read characters.
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while(c != 'q');
+    }
+}
