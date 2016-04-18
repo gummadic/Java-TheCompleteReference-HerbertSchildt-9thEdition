7a8aaeda3dd8c907b33d25a2895f88cee8738be4
Raven-Pro <m_117@outlook.com>
2016-04-18 16:27:46 -0500

    Add example for: Writing Console Output

---
+++ Chapter_13/src/P03_WritingConsoleOutput/WriteDemo.java
@@ -0,0 +1,12 @@
+package P03_WritingConsoleOutput;
+
+// Demostreate System.out.write().
+public class WriteDemo {
+    public static void main(String args[]) {
+        int b;
+        
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
