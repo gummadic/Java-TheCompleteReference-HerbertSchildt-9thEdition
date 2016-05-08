83f3aeb6ad282071f1f199138fd264e9176f3c7d
Raven-Pro <m_117@outlook.com>
2016-05-08 10:18:59 -0500

    Add example for: Applet Fundamentals

---
+++ Chapter_13/src/P07_AppletFundamentals/SimpleApplet.java
@@ -0,0 +1,15 @@
+package P07_AppletFundamentals;
+
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="SimpleApplet" width=200 height=60>
+</applet>
+*/
+
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("A Simple Applet", 20, 20);
+    }
+}
