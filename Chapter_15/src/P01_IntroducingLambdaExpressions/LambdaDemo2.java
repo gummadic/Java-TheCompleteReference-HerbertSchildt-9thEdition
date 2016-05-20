1a09d317032b07dd909d87b478b459165e8a5a9c
Raven-Pro <m_117@outlook.com>
2016-05-20 16:34:51 -0500

    Add examples for: Introducing Lambda Expressions

---
+++ Chapter_15/src/P01_IntroducingLambdaExpressions/LambdaDemo2.java
@@ -0,0 +1,24 @@
+package P01_IntroducingLambdaExpressions;
+
+// Demostrate a lambda expression that takes a parameter.
+
+// Another functional interface.
+interface NumericTest {
+    boolean test(int n);
+}
+
+public class LambdaDemo2 {
+    public static void main(String args[]) {
+        NumericTest isEven = (n) -> (n % 2) == 0;
+        
+        if(isEven.test(10)) System.out.println("10 is even.");
+        if(!isEven.test(9)) System.out.println("9 is not even.");
+        
+        // Now, use a lambda expression that test if a number
+        // is non-negative.
+        NumericTest isNonNeg = (n) -> n >= 0;
+        
+        if(isNonNeg.test(1)) System.out.println("1 is non-negative.");
+        if(!isNonNeg.test(-1)) System.out.println("-1 is negative.");
+    }
+}
---
+++ Chapter_15/src/P01_IntroducingLambdaExpressions/LambdaDemo3.java
@@ -0,0 +1,18 @@
+package P01_IntroducingLambdaExpressions;
+
+// Demostrate a lambda expression that takes two parameters.
+
+interface NumericTest2 {
+    boolean test(int n, int d);
+}
+
+public class LambdaDemo3 {
+    public static void main(String args[]) {
+        // This lambda expression determinaes if one number is
+        // a factor of another.
+        NumericTest2 isFactor = (n, d) -> (n % d) == 0;
+        
+        if(isFactor.test(10, 0)) System.out.println("2 is a factor of 10.");
+        if(!isFactor.test(10, 3)) System.out.println("3 is not a factor of 10.");
+    }
+}
