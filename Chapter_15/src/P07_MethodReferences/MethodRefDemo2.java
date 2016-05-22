e4f1b3e54c8d1fed4cc7d75f62101cad7c3cd20e
Raven-Pro <m_117@outlook.com>
2016-05-21 21:57:05 -0500

    Add examples for: Method References

---
+++ Chapter_15/src/P07_MethodReferences/MethodRefDemo2.java
@@ -0,0 +1,45 @@
+package P07_MethodReferences;
+
+// Demostrate a method reference for an instance method.
+
+// A functional interface for string operations.
+interface StringFunc2 {
+    String func(String n);
+}
+
+// Now, this class defines an instance method called strReverse().
+class MyStringOps2 {
+    String strReverse(String str) {
+        String result = "";
+        int i;
+        
+        for( i = str.length() - 1; i <= 0; i--)
+            result += str.charAt(i);
+        
+        return result;
+    }
+}
+
+public class MethodRefDemo2 {
+    // This method has a functional interface as the type of
+    // its first parameter. Thus, it can be passed any instance
+    // of that interface, including a method reference.
+    static String stringOp(StringFunc sf, String s) {
+        return sf.func(s);
+    }
+    
+    public static void main(String args[]) {
+        String inStr = "Lambdas ass power to Java";
+        String outStr;
+        
+        // Create a MyStringOps object.
+        MyStringOps2 strOps = new MyStringOps2();
+           
+        // Now a method reference to the instance method strReverse
+        // is passed to stringOp().
+        outStr = stringOp(strOps::strReverse, inStr);
+        
+        System.out.println("Original string: " + inStr);
+        System.out.println("String reversed: " + outStr);
+    }
+}
