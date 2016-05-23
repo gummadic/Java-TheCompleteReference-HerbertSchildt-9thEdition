179f53b5c46ff98f11fa9814fd4ab8b2639a72c2
Raven-Pro <m_117@outlook.com>
2016-05-22 21:18:00 -0500

    Add examples for: Special String Operations

---
+++ Chapter_16/src/P03_SpecialStringOperations/ConCat.java
@@ -0,0 +1,12 @@
+package P03_SpecialStringOperations;
+
+// Using concatenation to prevent long lines.
+class ConCat {
+    public static void main(String args[]) {
+        String longStr = "This could have been " +
+        "a very long line that would have " +
+        "wrapped around. But string concatenation " +
+        "prevents this.";
+        System.out.println(longStr);
+    }
+}
